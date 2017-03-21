0ca21a089e9bb7853733fd371e739d5d32d57cd9
devonhackley <devonhackley@gmail.com>
2017-03-21 14:21:22 -0700

    landing video background successfully loads

--- app/container/landing/index.js
+++ app/container/landing/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+require('./_landing.scss');
+
+require('angular').module('portfolio')
+.component('landing', {
+  template: require('./landing.html'),
+  controller: ['$log', '$location', function($log, $location){
+    this.$onInit = () => {
+      this.enterHandleSubmit = () => {
+        $log.log('we are leaving the landing page');
+        $location.path('/about');
+      };
+    };
+  }],
+});
